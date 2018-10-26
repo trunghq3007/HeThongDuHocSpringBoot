e29c1e41c4df571129ba170b467c2a34a9995a24
trunghq3007 <haquangtrungceo@gmail.com>
2018-10-26 09:13:36 +0700

    add project

---
+++ HeThongDuHocSpringBoot/src/main/java/com/cmc/HeThongDuHocSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.cmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeThongDuHocSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeThongDuHocSpringBootApplication.class, args);
+	}
+}
