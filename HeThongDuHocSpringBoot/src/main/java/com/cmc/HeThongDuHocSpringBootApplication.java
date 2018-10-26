107e1b18fe3ccb953279318aabae73e3b8b423c5
trunghq3007 <haquangtrungceo@gmail.com>
2018-10-26 09:23:30 +0700

    structure project follow MVC

--- HeThongDuHocSpringBoot/src/main/java/com/cmc/HeThongDuHocSpringBootApplication.java
+++ HeThongDuHocSpringBoot/src/main/java/com/cmc/HeThongDuHocSpringBootApplication.java
@@ -3,10 +3,23 @@ package com.cmc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author HQTrung
+ *
+ */
 @SpringBootApplication
 public class HeThongDuHocSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeThongDuHocSpringBootApplication.class, args);
-	}
+        /**
+         * Create by: HQTrung
+         * Create date: Oct 26, 2018
+         * Modifier: HQTrung
+         * Modified date: Oct 26, 2018
+         * Description: ....
+         * Version 1.0
+         * @param args
+         */
+        public static void main(String[] args) {
+                SpringApplication.run(HeThongDuHocSpringBootApplication.class, args);
+        }
 }
---
+++ HeThongDuHocSpringBoot/src/main/java/com/cmc/common/Util.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.cmc.common;
+
+/**
+ * @author HQTrung
+ *
+ */
+public class Util {
+
+}
